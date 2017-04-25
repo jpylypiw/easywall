5492faaeeea6a9086c1eb2c598ea7c5fee935fb7
KingJP <jochen@pylypiw.com>
2017-04-25 08:57:22 +0200

    Replaced Vendors Folder with Grunt

--- web/pages/footer.php
+++ web/pages/footer.php
@@ -81,8 +81,8 @@
 		</div>
 
 		<!-- jQuery first, then Tether, then Bootstrap JS. -->
-		<script src="vendors/jquery-3.1.1/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-		<script src="vendors/tether-1.4.0/js/tether.min.js" integrity="sha256-DbVrJ6TqTEoNLNIMTka94bhP3K3c+qiSus9nZJRwz58=" crossorigin="anonymous"></script>
-		<script src="vendors/bootstrap-4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<script src="bower_components/jquery/dist/jquery.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
+		<script src="bower_components/tether/dist/js/tether.min.js" integrity="sha256-gL1ibrbVcRIHKlCO5OXOPC/lZz/gpdApgQAzskqqXp8=" crossorigin="anonymous"></script>
+		<script src="bower_components/bootstrap/dist/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 	</body>
 </html>
