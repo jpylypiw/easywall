d1d8311c505915a36984e80f790fac0921eea27d
J. Pylypiw <jochen@pylypiw.com>
2019-01-19 17:50:46 +0100

    Initial Cleanup on new Python Rework Branch

--- web/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('pages/head.php');
-
-require('pages/home.php');
-
-require('pages/footer.php');
-
-?>
--- web/pages/home.php
+++
@@ -1,9 +0,0 @@
-<div class="cover-container">
-	<div class="inner cover">
-		<h1 class="cover-heading">EasyWall for IPtables.</h1>
-		<p class="lead">EasyWall is a tool for configuring firewalls. Currently, we are supporting the Linux IPTables firewall. EasyWall has been created with the modern languages PHP and BASH. Please note that EasyWall is currently still in alpha state.</p>
-		<p class="lead">
-			<a href="ports.php" class="btn btn-lg btn-secondary" role="button">Configure Ports</a>
-		</p>
-	</div>
-</div>
