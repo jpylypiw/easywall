e262878f33cfaf797a02bb5e15799b7956f69ae5
Jochen Pylypiw <jochen@pylypiw.com>
2019-04-14 00:19:20 +0200

    implemented some basic rules and tcp rules from folder

--- iptables.py
+++ iptables.py
@@ -24,41 +24,38 @@ class iptables(object):
             self.ip6tables_restore = self.config.getValue(
                 "EXEC", "ip6tables-restore")
 
-    def addrule(self, port, ruletype):
-        if ruletype == ruletype.TCP:
-            os.system(self.iptables + " -A INPUT -p tcp --dport " +
-                      str(port) + " --syn -m conntrack --ctstate NEW -j ACCEPT")
-        elif ruletype == ruletype.UCP:
-            os.system(self.iptables + " -A INPUT -p udp --dport " +
-                      str(port) + " -m conntrack --ctstate NEW -j ACCEPT")
+    def addPolicy(self, chain, target):
+        if target == "ACCEPT" or target == "DROP":
+            os.system(self.iptables + " -P " + chain + " " + target)
+            if self.ipv6 == True:
+                os.system(self.ip6tables + " -P " + chain + " " + target)
         else:
-            log.logging.error(
-                "Ruletype does not match any known ruletype. Ruletype given: " + ruletype)
+            log.logging.error("Invalid Target for addPolicy " + target)
+
+    def addAppend(self, chain, rule, onlyv6=False, onlyv4=False):
+        if onlyv6 == False:
+            os.system(self.iptables + " -A " + chain + " " + rule)
+        if self.ipv6 == True and onlyv4 == False:
+            os.system(self.ip6tables + " -A " + chain + " " + rule)
+
+    def flush(self, chain=""):
+        os.system(self.iptables + " -F " + chain)
+        if self.ipv6 == True:
+            os.system(self.ip6tables + " -F " + chain)
+
+    def deleteChain(self, chain=""):
+        os.system(self.iptables + " -X " + chain)
+        if self.ipv6 == True:
+            os.system(self.ip6tables + " -X " + chain)
 
     def reset(self):
         log.logging.info(
             "Beginning to reset the firewall rules and opening the firewall for every connection")
-
-        os.system(self.iptables + " -P INPUT ACCEPT")
-        os.system(self.iptables + " -P OUTPUT ACCEPT")
-        os.system(self.iptables + " -P FORWARD ACCEPT")
-        os.system(self.iptables + " -F")
-        os.system(self.iptables + " -X")
-        os.system(self.iptables + " -t nat -F")
-        os.system(self.iptables + " -t nat -X")
-        os.system(self.iptables + " -t mangle -F")
-        os.system(self.iptables + " -t mangle -X")
-
-        os.system(self.ip6tables + " -P INPUT ACCEPT")
-        os.system(self.ip6tables + " -P OUTPUT ACCEPT")
-        os.system(self.ip6tables + " -P FORWARD ACCEPT")
-        os.system(self.ip6tables + " -F")
-        os.system(self.ip6tables + " -X")
-        os.system(self.ip6tables + " -t nat -F")
-        os.system(self.ip6tables + " -t nat -X")
-        os.system(self.ip6tables + " -t mangle -F")
-        os.system(self.ip6tables + " -t mangle -X")
-
+        self.addPolicy("INPUT", "ACCEPT")
+        self.addPolicy("OUTPUT", "ACCEPT")
+        self.addPolicy("FORWARD", "ACCEPT")
+        self.flush()
+        self.deleteChain()
         log.logging.info(
             "The firewall rules for IPV4 and IPV6 have been successfully reset")
 
@@ -94,11 +91,11 @@ class iptables(object):
         if os.path.exists(filepath):
             log.logging.info("Restoring ipv4 rules...")
             filename = self.config.getValue("BACKUP", "ipv4filename")
-            os.system(self.iptables_restore + " -v < " +
+            os.system(self.iptables_restore + " < " +
                       filepath + "/" + filename)
 
             if self.ipv6 == True:
                 log.logging.info("Restoring ipv6 rules...")
                 filename = self.config.getValue("BACKUP", "ipv6filename")
-                os.system(self.ip6tables_restore + " -v < " +
+                os.system(self.ip6tables_restore + " < " +
                           filepath + "/" + filename)
