6020fb6a02e68fb85d766be9810b33deb2f8292a
Jochen Pylypiw <jochen@pylypiw.com>
2019-04-14 13:09:54 +0200

    acceptance enable switch added, only one instance of easywall is running, config bug for boolean fixed

--- acceptance.py
+++ acceptance.py
@@ -6,28 +6,30 @@ from time import sleep
 class acceptance(object):
     def __init__(self):
         self.config = config.config("config/config.ini")
+        self.enabled = bool(self.config.getValue("ACCEPTANCE", "enabled"))
         self.filename = self.config.getValue("ACCEPTANCE", "filename")
 
     def reset(self):
-        with open(self.filename, 'w') as accfile:
-            accfile.write('false')
+        if self.enabled == True:
+            with open(self.filename, 'w') as accfile:
+                accfile.write('false')
 
     def check(self):
-        seconds = self.config.getValue("ACCEPTANCE", "time")
-        log.logging.info(
-            "Starting Acceptance Check... waiting for " + seconds + " seconds")
-        sleep(int(seconds))
-        with open(self.filename, 'r') as accfile:
-            accepted = accfile.read()
-            if accepted == "true":
-                log.logging.info("Acceptance Process Result: Accepted")
-                return True
-            else:
-                log.logging.info(
-                    "Acceptance Process Result: Not Accepted (file content: " + accepted + ")")
-                return False
-
-    def accept(self):
-        # this function is only for testing purposes!
-        with open(self.filename, 'w') as accfile:
-            accfile.write('true')
+        if self.enabled == True:
+            seconds = int(self.config.getValue("ACCEPTANCE", "time"))
+            log.logging.info(
+                "Starting Acceptance Check... waiting for " +
+                str(seconds) + " seconds")
+            sleep(seconds)
+            with open(self.filename, 'r') as accfile:
+                accepted = accfile.read()
+                accepted = accepted.replace("\n", "")
+                if accepted == "true":
+                    log.logging.info("Acceptance Process Result: Accepted")
+                    return True
+                else:
+                    log.logging.info(
+                        "Acceptance Process Result: Not Accepted (file content: " + accepted + ")")
+                    return False
+        else:
+            return True
