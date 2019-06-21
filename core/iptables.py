97b97639090f8ed3de6ab87af8df9cb611809aa3
Jochen Pylypiw <jochen@pylypiw.com>
2019-06-21 21:17:51 +0200

    slight improvements in iptables class

--- core/iptables.py
+++ core/iptables.py
@@ -13,7 +13,7 @@ class iptables(object):
         self.iptables_bin_save = self.config.getValue("EXEC", "iptables-save")
         self.iptables_bin_restore = self.config.getValue(
             "EXEC", "iptables-restore")
-        if self.ipv6 == True:
+        if self.ipv6 is True:
             log.logging.debug("IPV6 is enabled")
             self.ip6tables_bin = self.config.getValue("EXEC", "ip6tables")
             self.ip6tables_bin_save = self.config.getValue(
@@ -25,40 +25,41 @@ class iptables(object):
         log.logging.debug("adding policy for chain " +
                           chain + " and target " + target)
         if target == "ACCEPT" or target == "DROP":
-            os.system(self.iptables_bin + " -P " + chain + " " + target)
-            if self.ipv6 == True:
-                os.system(self.ip6tables_bin + " -P " + chain + " " + target)
+            self.system_call(self.iptables_bin + " -P " + chain + " " + target)
+            if self.ipv6 is True:
+                self.system_call(
+                    self.ip6tables_bin + " -P " + chain + " " + target)
         else:
             log.logging.error("Invalid Target for addPolicy " + target)
 
     def addChain(self, chain):
         log.logging.debug("adding chain " + chain)
-        os.system(self.iptables_bin + " -N " + chain)
-        if self.ipv6 == True:
-            os.system(self.ip6tables_bin + " -N " + chain)
+        self.system_call(self.iptables_bin + " -N " + chain)
+        if self.ipv6 is True:
+            self.system_call(self.ip6tables_bin + " -N " + chain)
 
     def addAppend(self, chain, rule, onlyv6=False, onlyv4=False):
-        if onlyv4 == True or (onlyv6 == False and onlyv4 == False):
+        if onlyv4 is True or (onlyv6 is False and onlyv4 is False):
             log.logging.debug(
                 "adding append for ipv4, chain: " + chain + ", rule: " + rule)
-            os.system(self.iptables_bin + " -A " + chain + " " + rule)
-        if self.ipv6 == True and(
-                onlyv6 == True or(onlyv6 == False and onlyv4 == False)):
+            self.system_call(self.iptables_bin + " -A " + chain + " " + rule)
+        if self.ipv6 is True and(
+                onlyv6 is True or(onlyv6 is False and onlyv4 is False)):
             log.logging.debug(
                 "adding append for ipv6, chain: " + chain + ", rule: " + rule)
-            os.system(self.ip6tables_bin + " -A " + chain + " " + rule)
+            self.system_call(self.ip6tables_bin + " -A " + chain + " " + rule)
 
     def flush(self, chain=""):
         log.logging.debug("flushing iptables chain: " + chain)
-        os.system(self.iptables_bin + " -F " + chain)
-        if self.ipv6 == True:
-            os.system(self.ip6tables_bin + " -F " + chain)
+        self.system_call(self.iptables_bin + " -F " + chain)
+        if self.ipv6 is True:
+            self.system_call(self.ip6tables_bin + " -F " + chain)
 
     def deleteChain(self, chain=""):
         log.logging.debug("deleting chain " + chain)
-        os.system(self.iptables_bin + " -X " + chain)
-        if self.ipv6 == True:
-            os.system(self.ip6tables_bin + " -X " + chain)
+        self.system_call(self.iptables_bin + " -X " + chain)
+        if self.ipv6 is True:
+            self.system_call(self.ip6tables_bin + " -X " + chain)
 
     def reset(self):
         log.logging.debug("resetting iptables to empty configuration")
@@ -69,7 +70,7 @@ class iptables(object):
         self.deleteChain()
 
     def list(self):
-        os.system(self.iptables_bin + " -L")
+        self.system_call(self.iptables_bin + " -L")
 
     def save(self):
         log.logging.debug("Starting Firewall Rule Backup...")
@@ -80,21 +81,20 @@ class iptables(object):
         # backing up ipv4 iptables rules
         log.logging.debug("Backing up ipv4 rules...")
         filename = self.config.getValue("BACKUP", "ipv4filename")
-        with open(filepath + "/" + filename, 'w'):
-            pass
-        os.system(
-            self.iptables_bin_save + " | while read IN ; do echo $IN >> " +
-            filepath + "/" + filename + " ; done")
+        open(filepath + "/" + filename, 'w')
+        self.save_execute(self.iptables_bin_save, filepath, filename)
 
         # backing up ipv6 iptables rules
-        if self.ipv6 == True:
+        if self.ipv6 is True:
             log.logging.debug("Backing up ipv6 rules...")
             filename = self.config.getValue("BACKUP", "ipv6filename")
-            with open(filepath + "/" + filename, 'w'):
-                pass
-            os.system(
-                self.ip6tables_bin_save + " | while read IN ; do echo $IN >> " +
-                filepath + "/" + filename + " ; done")
+            open(filepath + "/" + filename, 'w')
+            self.save_execute(self.ip6tables_bin_save, filepath, filename)
+
+    def save_execute(self, binary, filepath, filename):
+        self.system_call(
+            binary + " | while read IN ; do echo $IN >> " + filepath + "/" +
+            filename + " ; done")
 
     def restore(self):
         log.logging.debug("Starting Firewall Rule Restore...")
@@ -103,11 +103,15 @@ class iptables(object):
 
         log.logging.debug("Restoring ipv4 rules...")
         filename = self.config.getValue("BACKUP", "ipv4filename")
-        os.system(self.iptables_bin_restore + " < " +
-                  filepath + "/" + filename)
+        self.system_call(self.iptables_bin_restore + " < " +
+                         filepath + "/" + filename)
 
-        if self.ipv6 == True:
+        if self.ipv6 is True:
             log.logging.debug("Restoring ipv6 rules...")
             filename = self.config.getValue("BACKUP", "ipv6filename")
-            os.system(self.ip6tables_bin_restore + " < " +
-                      filepath + "/" + filename)
+            self.system_call(self.ip6tables_bin_restore + " < " +
+                             filepath + "/" + filename)
+
+    def system_call(self, command):
+        # add security checks here
+        os.system(command)
