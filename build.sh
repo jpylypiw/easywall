e85d60e38129d6963c682ec9d744ecd8e65b154c
Jochen Pylypiw <jochen@pylypiw.com>
2019-06-29 21:03:47 +0200

    password generator

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 echo "Copy configuration..."
 cp config/config.ini config/config.ini.example
+newconfig=$(sed "s/username.*/username =/g" config/config.ini.example)
+echo "$newconfig" >config/config.ini.example
+newconfig=$(sed "s/password.*/password =/g" config/config.ini.example)
+echo "$newconfig" >config/config.ini.example
 
 echo "Run easywall..."
 /usr/bin/python3 core/easywall.py
