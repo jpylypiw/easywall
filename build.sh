fd56d9489ac3700f44ebbc2c83b07586e9481290
J. Pylypiw <jochen@pylypiw.com>
2019-06-30 16:34:50 +0200

    github oauth token added to config

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ newconfig=$(sed "s/bindip.*/bindip = 127.0.0.1/g" config/config.ini.example)
 echo "$newconfig" >config/config.ini.example
 newconfig=$(sed "s/bindport.*/bindport = 12227/g" config/config.ini.example)
 echo "$newconfig" >config/config.ini.example
+newconfig=$(sed "s/github_oauth.*/github_oauth = 12345/g" config/config.ini.example)
+echo "$newconfig" >config/config.ini.example
 
 echo "Run easywall..."
 /usr/bin/python3 core/easywall.py
