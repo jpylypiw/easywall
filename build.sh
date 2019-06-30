d13e49f1b02224a7374146ab7f24ce223f1e8d93
J. Pylypiw <jochen@pylypiw.com>
2019-06-30 13:27:25 +0200

    cfg for web port and ip address

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ newconfig=$(sed "s/username.*/username =/g" config/config.ini.example)
 echo "$newconfig" >config/config.ini.example
 newconfig=$(sed "s/password.*/password =/g" config/config.ini.example)
 echo "$newconfig" >config/config.ini.example
+newconfig=$(sed "s/bindip.*/bindip = 127.0.0.1/g" config/config.ini.example)
+echo "$newconfig" >config/config.ini.example
+newconfig=$(sed "s/bindport.*/bindport = 12227/g" config/config.ini.example)
+echo "$newconfig" >config/config.ini.example
 
 echo "Run easywall..."
 /usr/bin/python3 core/easywall.py
