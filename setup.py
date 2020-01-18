6f75692a358399f7e5b5829983123351afb70d38
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-01-18 22:29:11 +0100

    prepaded python package

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""this is the setup module for easywall"""
+from setuptools import setup
+
+
+def readme():
+    """include the readme as long_description"""
+    with open('README.md') as handler:
+        return handler.read()
+
+
+setup(
+    name='easywall',
+    version='0.0.4',
+    description='Software for simple control of Linux firewalls via configuration files written in Python',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: System :: Networking :: Firewalls',
+    ],
+    keywords="firewall iptables linux framework easywall",
+    url='https://github.com/jpylypiw/easywall',
+    author='Jochen Pylypiw',
+    author_email='jochen@pylypiw.com',
+    license='GPL-3.0',
+    packages=['easywall'],
+    install_requires=[
+        'watchdog',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest-cov',
+        'pytest-pythonpath',
+        'pytest',
+        'coverage'
+    ]
+)
