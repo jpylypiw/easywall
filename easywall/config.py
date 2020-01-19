101fe63dc71f5d7fb7cd83cdd955145af9eef831
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-01-19 14:18:13 +0100

    improved error handling in config lib

--- easywall/config.py
+++ easywall/config.py
@@ -15,7 +15,13 @@ class Config(object):
     def get_value(self, section, key):
         """Returns a value in a given section from the configuration file.
         Returns String, Float, Integer, Boolean"""
-        value = self.configlib[section][key]
+        value = ""
+        try:
+            value = self.configlib[section][key]
+        except KeyError:
+            print("Could not find key {} in section {}".format(key, section))
+            print("Valid sections are: ")
+            print("{}".format(self.get_sections()))
         if value in ["yes", "no", "true", "false", "on", "off"]:
             return self.configlib.getboolean(section, key)
         if is_int(value):
@@ -26,11 +32,26 @@ class Config(object):
 
     def get_sections(self):
         """Return a list of section names, excluding [DEFAULT]"""
-        return self.configlib.sections()
+        sections = ""
+        try:
+            sections = self.configlib.sections()
+        except Exception as exc:
+            print("Error while reading sections from config file: {}".format(exc))
+        return sections
 
     def set_value(self, section, key, value):
         """Writes a key, value pair into memory configuration and writes it to config file"""
         value = str(value)
-        self.configlib[section][key] = value
-        with open(self.configpath, 'w') as configfile:
-            self.configlib.write(configfile)
+        try:
+            self.configlib[section][key] = value
+        except Exception as exc:
+            print("Error while writing {} into key {} in section {}: {}".format(
+                value, key, section, exc))
+            print("Valid sections are: ")
+            print("{}".format(self.get_sections()))
+
+        try:
+            with open(self.configpath, 'w') as configfile:
+                self.configlib.write(configfile)
+        except Exception as exc:
+            print("Error while writing configuration into file {}: {}".format(self.configpath, exc))
