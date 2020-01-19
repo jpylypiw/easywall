8f2989804d3cc5666734fedfe0fb7db75b663204
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-01-19 01:41:26 +0100

    removed bad shell scripts

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPTDIR=$(dirname "$0")
-cd "$SCRIPTDIR" || exit 1
-/usr/bin/env systemctl stop easywall
-/usr/bin/env systemctl stop easywall-web
-/usr/bin/env git pull
-/usr/bin/env systemctl start easywall
-/usr/bin/env systemctl start easywall-web
