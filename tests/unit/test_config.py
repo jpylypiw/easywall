21df9828e26d84db01aff839c3a0626d4beca9e6
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-03-05 22:47:32 +0100

    imported tests from other project

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,16 +1,17 @@
 """
-this file is the test module for the config module
+TODO: Doku
 """
-from easywall.config import Config
-from easywall.utility import (create_file_if_not_exists, delete_file_if_exists,
-                              write_into_file)
+from configparser import ParsingError
 
+from oracle_reporting.config import Config
+from oracle_reporting.utility import (create_file_if_not_exists,
+                                      delete_file_if_exists, write_into_file)
 from tests import unittest
 
 
 class TestConfig(unittest.TestCase):
     """
-    this class contains all test functions for the config module
+    TODO: Doku
     """
 
     def setUp(self):
@@ -28,25 +29,66 @@ class TestConfig(unittest.TestCase):
     def tearDown(self):
         delete_file_if_exists("test.ini")
 
+    def test_constructor_file_not_found(self):
+        """
+        TODO: Doku
+        """
+        with self.assertRaises(FileNotFoundError):
+            Config("test2.ini")
+
+    def test_constructor_file_not_read(self):
+        """
+        TODO: Doku
+        """
+        create_file_if_not_exists("test.ini")
+        content = """[DEFAULT]
+        goodcontent = test
+        badcontent
+        """
+        write_into_file("test.ini", content)
+        with self.assertRaises(ParsingError):
+            Config("test.ini")
+
     def test_get_value_error(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.config.get_value("TEST", "notexistent"), "")
 
     def test_get_value_bool(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.config.get_value("TEST", "testboolean"), True)
 
     def test_get_value_int(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.config.get_value("TEST", "testint"), 1)
 
     def test_get_value_float(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.config.get_value(
             "TEST", "testfloat"), float(1.1))
 
     def test_get_sections(self):
+        """
+        TODO: Doku
+        """
         self.assertIn("TEST", self.config.get_sections())
 
     def test_set_value_success(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.config.set_value(
             "TEST", "teststring", "erfolg"), True)
 
     def test_set_value_fail_section(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.config.set_value("TEST2", "asd", "asd"), False)
--- tests/unit/test_log.py
+++ tests/unit/test_log.py
@@ -3,9 +3,8 @@ this file is the test module for the log module
 """
 from logging import CRITICAL, DEBUG, ERROR, INFO, NOTSET, WARNING
 
-from easywall.log import Log
-from easywall.utility import delete_file_if_exists
-
+from oracle_reporting.log import Log
+from oracle_reporting.utility import delete_file_if_exists
 from tests import unittest
 
 
@@ -22,19 +21,37 @@ class TestLog(unittest.TestCase):
         self.log.close_logging()
 
     def test_correct_level_debug(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.log.correct_level("DEBUG"), DEBUG)
 
     def test_correct_level_info(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.log.correct_level("INFO"), INFO)
 
     def test_correct_level_warning(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.log.correct_level("WARNING"), WARNING)
 
     def test_correct_level_error(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.log.correct_level("ERROR"), ERROR)
 
     def test_correct_level_critical(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.log.correct_level("CRITICAL"), CRITICAL)
 
     def test_correct_level_unknown(self):
+        """
+        TODO: Doku
+        """
         self.assertEqual(self.log.correct_level("SOMETHING"), NOTSET)
