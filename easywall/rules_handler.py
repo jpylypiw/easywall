f2c3aab86605c89afe7b48b760a85d7a72c71a7d
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-03-05 22:43:43 +0100

    added new rules_handler

---
+++ easywall/rules_handler.py
@@ -0,0 +1,63 @@
+"""
+TODO: Doku
+"""
+from easywall.config import Config
+from easywall.utility import (create_file_if_not_exists,
+                              create_folder_if_not_exists, file_get_contents,
+                              write_into_file)
+
+
+class RulesHandler(object):
+    """
+    TODO: Doku
+    """
+
+    def __init__(self, cfg: Config) -> None:
+        self.cfg = cfg
+        self.rulesfolder = "rules"
+        self.types = ["blacklist", "whitelist", "tcp", "udp", "custom"]
+        self.states = ["current", "new", "backup"]
+
+    def get_current_rules(self, ruletype: str) -> list:
+        """
+        TODO: Doku
+        """
+        file_get_contents("{}/current/{}".format(self.rulesfolder, ruletype)).splitlines()
+
+    def backup_current_rules(self) -> None:
+        """
+        TODO: Doku
+        """
+        self.copy_rules("current", "backup")
+
+    def apply_new_rules(self) -> None:
+        """
+        TODO: Doku
+        """
+        self.copy_rules("new", "current")
+
+    def rollback_from_backup(self) -> None:
+        """
+        TODO: Doku
+        """
+        self.copy_rules("backup", "current")
+
+    def copy_rules(self, source: str, dest: str) -> None:
+        """
+        TODO: Doku
+        """
+        for ruletype in self.types:
+            content = file_get_contents("{}/{}/{}".format(self.rulesfolder, source, ruletype))
+            write_into_file("{}/{}/{}".format(self.rulesfolder, dest, ruletype), content)
+
+    def rules_firstrun(self) -> None:
+        """
+        TODO: Doku
+        """
+        create_folder_if_not_exists(self.rulesfolder)
+
+        for state in self.states:
+            create_folder_if_not_exists("{}/{}".format(self.rulesfolder, state))
+
+            for ruletype in self.types:
+                create_file_if_not_exists("{}/{}/{}".format(self.rulesfolder, state, ruletype))
