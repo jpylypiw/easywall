c815abbe027fb5c83c676fdd642ac82b4d7bec7c
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-03-14 23:28:32 +0100

    firstrun implemented in web

--- easywall_web/__main__.py
+++ easywall_web/__main__.py
@@ -6,6 +6,8 @@ from flask import Flask
 
 from easywall.config import Config
 from easywall.log import Log
+from easywall.rules_handler import RulesHandler
+from easywall.utility import folder_exists
 from easywall_web.apply import apply, apply_save
 from easywall_web.blacklist import blacklist, blacklist_save
 from easywall_web.custom import custom, custom_save
@@ -134,6 +136,11 @@ class Main(object):
         self.log = Log(loglevel, to_stdout, to_files, logpath, logfile)
 
         info("starting easywall-web")
+
+        self.is_first_run = not folder_exists("rules")
+        self.rules_handler = RulesHandler()
+        if self.is_first_run:
+            self.rules_handler.rules_firstrun()
 
         if debug is True:
             port = self.cfg.get_value("WEB", "bindport")
