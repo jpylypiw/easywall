a37c7690fba6f70c2c628cd99544768ce1bbd677
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-08-22 14:27:13 +0200

    remove asserts in test_utility class

--- tests/easywall/test_utility.py
+++ tests/easywall/test_utility.py
@@ -1,6 +1,4 @@
-"""
-this file is the test module for the utility module
-"""
+"""this file is the test module for the utility module."""
 import os
 from datetime import datetime, timedelta
 from time import sleep
@@ -15,14 +13,10 @@ from tests import unittest
 
 
 class TestUtility(unittest.TestCase):
-    """
-    this class contains all test functions for the utility module
-    """
+    """this class contains all test functions for the utility module."""
 
     def test_folder(self) -> None:
-        """
-        TODO: Doku
-        """
+        """TODO: Doku."""
         foldername = "pytestfolder"
         create_folder_if_not_exists(foldername)
         sleep(1)
@@ -33,51 +27,41 @@ class TestUtility(unittest.TestCase):
         self.assertFalse(folder_exists(foldername))
 
     def test_file(self) -> None:
-        """
-        TODO: Doku
-        """
-        assert not file_exists("testfile")
+        """TODO: Doku."""
+        self.assertFalse(file_exists("testfile"))
         create_file_if_not_exists("testfile")
-        assert file_exists("testfile")
+        self.assertTrue(file_exists("testfile"))
         write_into_file("testfile", "testcontent")
-        assert file_get_contents("testfile") == "testcontent"
+        self.assertEqual(file_get_contents("testfile"), "testcontent")
         self.assertGreater(len(get_abs_path_of_filepath("testfile")), 0)
         rename_file("testfile", "testfilenew")
-        assert not file_exists("testfile")
-        assert file_exists("testfilenew")
+        self.assertFalse(file_exists("testfile"))
+        self.assertTrue(file_exists("testfilenew"))
         delete_file_if_exists("testfilenew")
-        assert not file_exists("testfile")
-        assert not file_exists("testfilenew")
+        self.assertFalse(file_exists("testfile"))
+        self.assertFalse(file_exists("testfilenew"))
 
     def test_float_int(self) -> None:
-        """
-        TODO: Doku
-        """
-        assert is_float(1)
-        assert is_float(1.1)
-        assert is_float(1.0)
-        assert not is_float("test")
-        assert is_int(1)
-        assert is_int(1.0)
-        assert not is_int(1.1)
-        assert not is_int("test")
+        """TODO: Doku."""
+        self.assertTrue(is_float(1))
+        self.assertTrue(is_float(1.1))
+        self.assertTrue(is_float(1.0))
+        self.assertFalse(is_float("test"))
+        self.assertTrue(is_int(1))
+        self.assertTrue(is_int(1.0))
+        self.assertFalse(is_int(1.1))
+        self.assertFalse(is_int("test"))
 
     def test_csv(self) -> None:
-        """
-        TODO: Doku
-        """
+        """TODO: Doku."""
         self.assertListEqual(csv_to_array("var1;var2", ";"), ["var1", "var2"])
 
     def test_urlencode(self) -> None:
-        """
-        TODO: Doku
-        """
+        """TODO: Doku."""
         self.assertEqual(urlencode("asd asd +"), "asd%20asd%20%2B")
 
     def test_time_duration_diff(self) -> None:
-        """
-        TODO: Doku
-        """
+        """TODO: Doku."""
         date1 = datetime.now()
         date2 = date1 + timedelta(0, 5)
         self.assertEqual(time_duration_diff(date1, date2), "5 seconds")
@@ -86,15 +70,11 @@ class TestUtility(unittest.TestCase):
         self.assertEqual(time_duration_diff(date1, date2), "1 second")
 
     def test_execute_os_command(self) -> None:
-        """
-        TODO: Doku
-        """
+        """TODO: Doku."""
         self.assertTrue(execute_os_command("touch testfile"))
         self.assertTrue(file_exists("testfile"))
         delete_file_if_exists("testfile")
 
     def test_execute_os_command_error(self) -> None:
-        """
-        TODO: Doku
-        """
+        """TODO: Doku."""
         self.assertFalse(execute_os_command("/bin/false"))
