8512439d2c291ab6b95a57e123011ea9affcad26
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-09-13 21:18:10 +0200

    add a new test

--- tests/easywall/test_rules_handler.py
+++ tests/easywall/test_rules_handler.py
@@ -42,6 +42,16 @@ class TestRulesHandler(unittest.TestCase):
         self.rules.apply_new_rules()
         self.assertEqual(self.rules.get_current_rules("tcp"), ["80", "443"])
 
+    def test_get_rules_for_web(self) -> None:
+        """TODO: Doku."""
+        ports = ["80", "443"]
+        self.rules.save_new_rules("tcp", ports)
+        self.rules.apply_new_rules()
+        self.assertEqual(self.rules.get_rules_for_web("tcp"), ["80", "443"])
+        ports = ["80", "443", "8080"]
+        self.rules.save_new_rules("tcp", ports)
+        self.assertEqual(self.rules.get_rules_for_web("tcp"), ["80", "443", "8080"])
+
     def test_rollback_from_backup(self) -> None:
         """TODO: Doku."""
         ports = ["80", "443"]
@@ -54,15 +64,14 @@ class TestRulesHandler(unittest.TestCase):
         self.rules.rollback_from_backup()
         self.assertEqual(self.rules.get_current_rules("tcp"), ["80", "443"])
 
-    def test_get_rules_for_web(self) -> None:
+    def test_diff_new_current(self) -> None:
         """TODO: Doku."""
-        ports = ["80", "443"]
-        self.rules.save_new_rules("tcp", ports)
+        self.rules.save_new_rules("tcp", ["123", "1234"])
         self.rules.apply_new_rules()
-        self.assertEqual(self.rules.get_rules_for_web("tcp"), ["80", "443"])
-        ports = ["80", "443", "8080"]
-        self.rules.save_new_rules("tcp", ports)
-        self.assertEqual(self.rules.get_rules_for_web("tcp"), ["80", "443", "8080"])
+        self.rules.save_new_rules("tcp", ["1337"])
+        self.assertTrue(self.rules.diff_new_current("tcp"))
+        self.rules.apply_new_rules()
+        self.assertFalse(self.rules.diff_new_current("tcp"))
 
     def test_save_new_rules(self) -> None:
         """TODO: Doku."""
