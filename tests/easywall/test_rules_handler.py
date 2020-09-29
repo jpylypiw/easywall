50d41d21cbd55aac2b98ad272875776510335790
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-09-30 00:39:05 +0200

    fix ports in tests

--- tests/easywall/test_rules_handler.py
+++ tests/easywall/test_rules_handler.py
@@ -13,48 +13,123 @@ class TestRulesHandler(unittest.TestCase):
 
     def test_get_current_rules(self) -> None:
         """TODO: Doku."""
-        ports = ["80", "443"]
+        ports: list = []
+        entry: dict = {}
+        entry["description"] = "test"
+        entry["port"] = "80"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "443"
+        entry["ssh"] = False
+        ports.append(entry)
         self.rules.save_new_rules("tcp", ports)
         self.rules.apply_new_rules()
-        self.assertEqual(self.rules.get_current_rules("tcp"), ["80", "443"])
+        self.assertEqual(self.rules.get_current_rules("tcp"), ports)
 
     def test_get_new_rules(self) -> None:
         """TODO: Doku."""
-        ports = ["80", "443"]
+        ports: list = []
+        entry: dict = {}
+        entry["description"] = "test"
+        entry["port"] = "80"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "443"
+        entry["ssh"] = False
+        ports.append(entry)
         self.rules.save_new_rules("tcp", ports)
-        self.assertEqual(self.rules.get_new_rules("tcp"), ["80", "443"])
+        self.assertEqual(self.rules.get_new_rules("tcp"), ports)
 
     def test_backup_current_rules(self) -> None:
         """TODO: Doku."""
-        ports = ["80", "443"]
+        ports: list = []
+        entry: dict = {}
+        entry["description"] = "test"
+        entry["port"] = "80"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "443"
+        entry["ssh"] = False
+        ports.append(entry)
         self.rules.save_new_rules("tcp", ports)
         self.rules.apply_new_rules()
         self.rules.backup_current_rules()
-        self.assertEqual(self.rules.get_backup_rules("tcp"), ["80", "443"])
+        self.assertEqual(self.rules.get_backup_rules("tcp"), ports)
 
     def test_apply_new_rules(self) -> None:
         """TODO: Doku."""
-        ports = ["80", "443"]
+        ports: list = []
+        entry: dict = {}
+        entry["description"] = "test"
+        entry["port"] = "80"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "443"
+        entry["ssh"] = False
+        ports.append(entry)
         self.rules.save_new_rules("tcp", [])
         self.rules.apply_new_rules()
         self.assertEqual(self.rules.get_current_rules("tcp"), [])
         self.rules.save_new_rules("tcp", ports)
         self.rules.apply_new_rules()
-        self.assertEqual(self.rules.get_current_rules("tcp"), ["80", "443"])
+        self.assertEqual(self.rules.get_current_rules("tcp"), ports)
 
     def test_get_rules_for_web(self) -> None:
         """TODO: Doku."""
-        ports = ["80", "443"]
+        ports: list = []
+        entry: dict = {}
+        entry["description"] = "test"
+        entry["port"] = "80"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "443"
+        entry["ssh"] = False
+        ports.append(entry)
         self.rules.save_new_rules("tcp", ports)
         self.rules.apply_new_rules()
-        self.assertEqual(self.rules.get_rules_for_web("tcp"), ["80", "443"])
-        ports = ["80", "443", "8080"]
+        self.assertEqual(self.rules.get_rules_for_web("tcp"), ports)
+        ports = []
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "80"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "443"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "8080"
+        entry["ssh"] = False
+        ports.append(entry)
         self.rules.save_new_rules("tcp", ports)
-        self.assertEqual(self.rules.get_rules_for_web("tcp"), ["80", "443", "8080"])
+        self.assertEqual(self.rules.get_rules_for_web("tcp"), ports)
 
     def test_rollback_from_backup(self) -> None:
         """TODO: Doku."""
-        ports = ["80", "443"]
+        ports: list = []
+        entry: dict = {}
+        entry["description"] = "test"
+        entry["port"] = "80"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "443"
+        entry["ssh"] = False
+        ports.append(entry)
         self.rules.save_new_rules("tcp", ports)
         self.rules.apply_new_rules()
         self.rules.backup_current_rules()
@@ -62,18 +137,46 @@ class TestRulesHandler(unittest.TestCase):
         self.rules.apply_new_rules()
         self.assertEqual(self.rules.get_current_rules("tcp"), [])
         self.rules.rollback_from_backup()
-        self.assertEqual(self.rules.get_current_rules("tcp"), ["80", "443"])
+        self.assertEqual(self.rules.get_current_rules("tcp"), ports)
 
     def test_diff_new_current(self) -> None:
         """TODO: Doku."""
-        self.rules.save_new_rules("tcp", ["123", "1234"])
+        ports: list = []
+        entry: dict = {}
+        entry["description"] = "test"
+        entry["port"] = "123"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "1234"
+        entry["ssh"] = False
+        ports.append(entry)
+        self.rules.save_new_rules("tcp", ports)
         self.rules.apply_new_rules()
-        self.rules.save_new_rules("tcp", ["1337"])
+        ports = []
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "1337"
+        entry["ssh"] = False
+        ports.append(entry)
+        self.rules.save_new_rules("tcp", ports)
         self.assertTrue(self.rules.diff_new_current("tcp"))
         self.rules.apply_new_rules()
         self.assertFalse(self.rules.diff_new_current("tcp"))
 
     def test_save_new_rules(self) -> None:
         """TODO: Doku."""
-        self.rules.save_new_rules("tcp", ["80", "443"])
-        self.assertEqual(self.rules.get_new_rules("tcp"), ["80", "443"])
+        ports: list = []
+        entry: dict = {}
+        entry["description"] = "test"
+        entry["port"] = "80"
+        entry["ssh"] = False
+        ports.append(entry)
+        entry = {}
+        entry["description"] = "test"
+        entry["port"] = "443"
+        entry["ssh"] = False
+        ports.append(entry)
+        self.rules.save_new_rules("tcp", ports)
+        self.assertEqual(self.rules.get_new_rules("tcp"), ports)
